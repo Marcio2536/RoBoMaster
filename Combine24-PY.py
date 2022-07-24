e96e68cea4ec27bc9c4892a8502ba2bdc30cf7dd
MCL7D9 <mcl7d9@outlook.com>
2022-07-24 10:41:20 +0800

    Update Combine24-PY.py

--- Combine24-PY.py
+++ Combine24-PY.py
@@ -1,25 +1,21 @@
-import re
+from ctypes.wintypes import WORD
 import random
-enter=input("Enter 4 number and a sign")
-sign=enter[4]
-ele=[]
-enter_list=[0,0,0,0]
-reslt=0
-solution=""
+
+word=input("Input 4 number and a sign")
+sign=word[4]
+num_list=[]
 length=0
+
 def analyze():
-    global length
     for i in range(4):
-        enter_list[i]=enter[i]
-    for i in range(4):
+        num_list[i]=word[i]
         for j in range(4):
-            enter_list.append(str(enter_list[i])+str(enter_list[j]))
-    length = len(enter_list)
+            num_list.append(str(word_list[i])+str(word_list[j]))
+    length=len(num_list)
     for i in range(length):
-        enter_list[i]=int(enter_list[i])
+        num_list[i]=int(num_list[i])
     if sign=="+":
-        pass
-        #plus()
+        plus()
     elif sign=="-":
         pass
         #minus()
@@ -29,16 +25,17 @@ def analyze():
     else:
         pass
         #divid()
-def plus():
-    plist=enter_list
-    length=len(plist)
-    plist.sort(reverse=True)
-    for i in range(length):
-        if plist[i]>24:
-            del plist[i]
-        for j in range(length):
-            reslt=plist[i]+plist[j]
-            if reslt==24:
-                solution=str(i)+"+"+str(j)+"="+str(i*j)
-    print(enter_list)
+class calculate:
+    def plus():
+        plist=word_list
+        length=len(plist)
+        plist.sort(reverse=True)
+        for i in range(length):
+            if plist[i]>24:
+                del plist[i]
+            for j in range(length):
+                reslt=plist[i]+plist[j]
+                if reslt==24:
+                    solution=str(i)+"+"+str(j)+"="+str(i*j)
+        print(word_list)
 analyze()

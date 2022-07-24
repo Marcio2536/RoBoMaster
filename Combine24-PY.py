819daf606fc8581800bc02c20dbbb60a5d8d456c
MCL7D9 <mcl7d9@outlook.com>
2022-07-24 11:12:00 +0800

    Update Combine24-PY.py

--- Combine24-PY.py
+++ Combine24-PY.py
@@ -5,17 +5,18 @@ word=input("Input 4 number and a sign")
 sign=word[4]
 num_list=[]
 length=0
+solution=""
 
 def analyze():
     for i in range(4):
         num_list[i]=word[i]
         for j in range(4):
-            num_list.append(str(word_list[i])+str(word_list[j]))
+            num_list.append(str(num_list[i])+str(num_list[j]))
     length=len(num_list)
     for i in range(length):
         num_list[i]=int(num_list[i])
     if sign=="+":
-        plus()
+        calculate.plus()
     elif sign=="-":
         pass
         #minus()
@@ -27,15 +28,19 @@ def analyze():
         #divid()
 class calculate:
     def plus():
-        plist=word_list
+        plist=num_list
         length=len(plist)
         plist.sort(reverse=True)
         for i in range(length):
             if plist[i]>24:
                 del plist[i]
+        length=len(plist)
+        for i in range(plist):
             for j in range(length):
                 reslt=plist[i]+plist[j]
                 if reslt==24:
                     solution=str(i)+"+"+str(j)+"="+str(i*j)
-        print(word_list)
+        print(solution)
+    def minus():
+        s
 analyze()

61abb3280272db224edd17e257f6829a92a5fa09
MCL7D9 <101371148+MCL7D9@users.noreply.github.com>
2022-08-02 15:45:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+command=""
+com_list=""
+varible=""
+action=""
+print("Welcome To RoBoMaster Command Line")
+while True:
+    command=input("RoBoMaster $")
+    com_list=command.split()
+    action=com_list[0]
+    if len(com_list)==2:
+        varible=com_list[1]
+        if action=="setssid":
+            ssid=varible
+            print("Sucessfully Set SSID")
+        elif action=="setpassword":
+            password=varible
+            print("Sucessfully Set Password")
+        else:
+            print("Error: Unrecognised Command")
+    elif len(com_list)==1:
+        if command=="setdefault":
+            pass
+        else:
+            print("Error: Unrecognised Command")
+    else:
+        print("Error: Unrecognised Command")
+            

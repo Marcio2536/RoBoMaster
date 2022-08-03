3790f6ea8badecef4784a51b4a09fa9d96d97ffd
MCL7D9 <mcl7d9@outlook.com>
2022-08-03 14:24:05 +0800

    Solved Problem Of Two Words SSID

--- main.py
+++ main.py
@@ -1,45 +1,43 @@
+from more_itertools import strip
 import Robot_Control.Control as control
 command=""
 com_list=""
-varible=""
+variable=""
 action=""
 error=["Invalid Variable","Unrecognised Command"]
 robo_var={"speed":10,"mode":"free"}
-
 print("Welcome To RoBoMaster Command Line")
+
 while True:
     command=input("RoBoMaster $")
+    ind=command.index(" ")
+    ind+=1
     com_list=command.split()
     action=com_list[0]
-    if len(com_list)==2:
-        varible=com_list[1]
-        if action=="setssid":
-            ssid=varible
-            print("Sucessfully Set SSID")
-        elif action=="setpassword":
-            password=varible
-            print("Sucessfully Set Password")
-        elif action=="combine24":
-            if varible=="r":
-                pass
-            elif varible=="c":
-                pass
-        elif action=="setspeed":
-            if type(varible)=="int" or type(varible)=="float":
-                robo_var["speed"]=varible
-                print("RoBo Speed Set")
-            else:
-                print("Error:",error[0])
-        elif action=="epconnect":
-            message=control.connect(ssid,password)
-            print(message)
-        else:
-            print("Error:",error[1])
-    elif len(com_list)==1:
-        if command=="setdefault":
+    variable=com_list[1]
+    if action=="setssid":
+        command=command+"\""
+        ssid=command[ind:-1]
+        print("Sucessfully Set SSID",ssid)
+    elif action=="setpassword":
+        password=variable
+        print("Sucessfully Set Password")
+    elif action=="combine24":
+        if variable=="r":
             pass
+        elif variable=="c":
+            pass
+    elif action=="setspeed":
+        if type(variable)=="int" or type(variable)=="float":
+            robo_var["speed"]=variable
+            print("RoBo Speed Set")
         else:
-            print("Error:",error[1])
+            print("Error:",error[0])
+    elif action=="epconnect":
+        message=control.connect(ssid,password)
+        print(message)
+    elif command=="setdefault":
+        pass
     else:
         print("Error:",error[1])
             
